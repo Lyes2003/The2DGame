324fbdc67d75ecd1da54985716222da5ad0a027d
lyes <chikh.lyes@courrier.uqam.ca>
2025-03-24 17:13:53 -0400

    ajout d'autre variable pour charger les images

--- 2DGame/src/mainproject/entity/Entity.java
+++ 2DGame/src/mainproject/entity/Entity.java
@@ -7,7 +7,8 @@ public class Entity {
     public int x, y;
     public int speed;
 
-    public BufferedImage haut1, haut2, bas1, bas2, gauche1, gauche2, droite1, droite2;
+    public BufferedImage haut1, haut2, haut3, haut4, haut5, haut6, bas1, bas2, bas3, bas4, bas5, bas6, gauche1, gauche2, gauche3, gauche4, gauche5, gauche6,
+            droite1, droite2, droite3, droite4, droite5, droite6;
     public String direction;
 
     public int spriteCounter = 0;
