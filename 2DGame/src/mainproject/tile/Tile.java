4064acd15515f16d60606e2410738c66ab9d3098
lyes <chikh.lyes@courrier.uqam.ca>
2025-03-26 02:34:59 -0400

    suppression des constructeur de la classe parce que inutile pour le moment.

--- 2DGame/src/mainproject/tile/Tile.java
+++ 2DGame/src/mainproject/tile/Tile.java
@@ -6,12 +6,4 @@ public class Tile {
     public BufferedImage image; // L'image de la tuile
     public boolean collision = false; // Indique si la tuile est "solide" (pour gérer les collisions plus tard)
 
-    public Tile(BufferedImage image) {
-        this.image = image;
-    }
-
-    public Tile(BufferedImage image, boolean collision) {
-        this.image = image;
-        this.collision = collision;
-    }
 }
