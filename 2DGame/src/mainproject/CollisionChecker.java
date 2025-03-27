e6e1d7d90b870c425a885e67528e1c18227250ac
lyes <chikh.lyes@courrier.uqam.ca>
2025-03-27 02:33:43 -0400

    Modification de la methode checkTile() pour gere la collision sur different tuile et corriger l'erreur de out of bond a cause de la nouvelle map de test

--- 2DGame/src/mainproject/CollisionChecker.java
+++ 2DGame/src/mainproject/CollisionChecker.java
@@ -12,52 +12,86 @@ public class CollisionChecker {
 
     public void checkTile(Entity entity) {
 
-        int entityLeftWorldX = entity.worldx +entity.hitbox.x;
+        int entityLeftWorldX = entity.worldx + entity.hitbox.x;
         int entityRightWorldX = entity.worldx + entity.hitbox.x + entity.hitbox.width;
-        int entityTopWorldy = entity.worldy + entity.hitbox.y;
-        int entityBottomWorldy = entity.worldy + entity.hitbox.y + entity.hitbox.height;
-
-        int entityLeftCol = entityLeftWorldX / gp.tileSize;
-        int entityRightCol = entityRightWorldX / gp.tileSize;
-        int entityTopRow = entityTopWorldy / gp.tileSize;
-        int entityBottomRow = entityBottomWorldy / gp.tileSize;
+        int entityTopWorldY = entity.worldy + entity.hitbox.y;
+        int entityBottomWorldY = entity.worldy + entity.hitbox.y + entity.hitbox.height;
 
         int tileNum1, tileNum2;
 
-        switch(entity.direction){
-            case "haut" :
-                entityTopRow = (entityTopWorldy - entity.speed) / gp.tileSize;
-                tileNum1 = gp.tileManager.mapTileNum[entityLeftCol][entityTopRow];
-                tileNum2 = gp.tileManager.mapTileNum[entityRightCol][entityTopRow];
-                if(gp.tileManager.tileTypes[tileNum1].collision == true || gp.tileManager.tileTypes[tileNum2].collision == true ){
-                    entity.collisionOn = true;
-                }
-                break;
-            case "bas" :
-                entityBottomRow = (entityBottomWorldy + entity.speed) / gp.tileSize;
-                tileNum1 = gp.tileManager.mapTileNum[entityLeftCol][entityBottomRow];
-                tileNum2 = gp.tileManager.mapTileNum[entityRightCol][entityBottomRow];
-                if(gp.tileManager.tileTypes[tileNum1].collision == true || gp.tileManager.tileTypes[tileNum2].collision == true ){
-                    entity.collisionOn = true;
-                }
-                break;
-            case "gauche" :
-                entityLeftCol = (entityLeftWorldX - entity.speed) / gp.tileSize;
-                tileNum1 = gp.tileManager.mapTileNum[entityLeftCol][entityTopRow];
-                tileNum2 = gp.tileManager.mapTileNum[entityLeftCol][entityBottomRow];
-                if(gp.tileManager.tileTypes[tileNum1].collision == true || gp.tileManager.tileTypes[tileNum2].collision == true ){
-                    entity.collisionOn = true;
-                }
-                break;
-            case "droite" :
-                entityRightCol = (entityRightWorldX + entity.speed) / gp.tileSize;
-                tileNum1 = gp.tileManager.mapTileNum[entityLeftCol][entityTopRow];
-                tileNum2 = gp.tileManager.mapTileNum[entityRightCol][entityBottomRow];
-                if(gp.tileManager.tileTypes[tileNum1].collision == true || gp.tileManager.tileTypes[tileNum2].collision == true ){
-                    entity.collisionOn = true;
-                }
-                break;
-        }
+        switch (entity.direction) {
+            case "haut": {
+                int leftCol = entityLeftWorldX / gp.tileSize;
+                int rightCol = entityRightWorldX / gp.tileSize;
+                int topRow = (entityTopWorldY - entity.speed) / gp.tileSize;
+                if (isInBounds(leftCol, topRow) && isInBounds(rightCol, topRow)) {
+                    for (int layer : new int[]{0, 2}) {
+                        tileNum1 = gp.tileManager.mapTileNum[layer][leftCol][topRow];
+                        tileNum2 = gp.tileManager.mapTileNum[layer][rightCol][topRow];
+                        if ((tileNum1 >= 0 && gp.tileManager.tileTypes[tileNum1].collision) ||
+                                (tileNum2 >= 0 && gp.tileManager.tileTypes[tileNum2].collision)) {
+                            entity.collisionOn = true;
+                        }
 
+                    }
+                }
+                break;
+            }
+            case "bas": {
+                int leftCol = entityLeftWorldX / gp.tileSize;
+                int rightCol = entityRightWorldX / gp.tileSize;
+                int bottomRow = (entityBottomWorldY + entity.speed) / gp.tileSize;
+                if (isInBounds(leftCol, bottomRow) && isInBounds(rightCol, bottomRow)) {
+                    for (int layer : new int[]{0, 2}) {
+                        tileNum1 = gp.tileManager.mapTileNum[layer][leftCol][bottomRow];
+                        tileNum2 = gp.tileManager.mapTileNum[layer][rightCol][bottomRow];
+                        if ((tileNum1 >= 0 && gp.tileManager.tileTypes[tileNum1].collision) ||
+                                (tileNum2 >= 0 && gp.tileManager.tileTypes[tileNum2].collision)) {
+                            entity.collisionOn = true;
+                        }
+
+                    }
+                }
+                break;
+            }
+            case "gauche": {
+                int topRow = entityTopWorldY / gp.tileSize;
+                int bottomRow = entityBottomWorldY / gp.tileSize;
+                int leftCol = (entityLeftWorldX - entity.speed) / gp.tileSize;
+                if (isInBounds(leftCol, topRow) && isInBounds(leftCol, bottomRow)) {
+                    for (int layer : new int[]{0, 2}) {
+                        tileNum1 = gp.tileManager.mapTileNum[layer][leftCol][topRow];
+                        tileNum2 = gp.tileManager.mapTileNum[layer][leftCol][bottomRow];
+                        if ((tileNum1 >= 0 && gp.tileManager.tileTypes[tileNum1].collision) ||
+                                (tileNum2 >= 0 && gp.tileManager.tileTypes[tileNum2].collision)) {
+                            entity.collisionOn = true;
+                        }
+
+                    }
+                }
+                break;
+            }
+            case "droite": {
+                int topRow = entityTopWorldY / gp.tileSize;
+                int bottomRow = entityBottomWorldY / gp.tileSize;
+                int rightCol = (entityRightWorldX + entity.speed) / gp.tileSize;
+                if (isInBounds(rightCol, topRow) && isInBounds(rightCol, bottomRow)) {
+                    for (int layer : new int[]{0, 2}) {
+                        tileNum1 = gp.tileManager.mapTileNum[layer][rightCol][topRow];
+                        tileNum2 = gp.tileManager.mapTileNum[layer][rightCol][bottomRow];
+                        if ((tileNum1 >= 0 && gp.tileManager.tileTypes[tileNum1].collision) ||
+                                (tileNum2 >= 0 && gp.tileManager.tileTypes[tileNum2].collision)) {
+                            entity.collisionOn = true;
+                        }
+
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    private boolean isInBounds(int col, int row) {
+        return col >= 0 && col < gp.maxWorldCol && row >= 0 && row < gp.maxWorldRow;
     }
 }
